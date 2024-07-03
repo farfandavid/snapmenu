bf7eb04f0e65e1ad6e5b0ec2d6623d89e0f39974
farfandavid <39929763@fi.unju.edu.ar>
2024-07-03 02:45:02 -0300

    mp payment data

--- src/pages/api/payment/webhook.ts
+++ src/pages/api/payment/webhook.ts
@@ -3,11 +3,25 @@ import { payment } from "../../../config/mpconfig";
 
 export const POST: APIRoute = async ({ request, url }) => {
     const id = url.searchParams.get("id");
+    console.log("id:", id);
     const res = await request.json();
-    console.log("body", res);
-    const paymentData = await payment.get({
-        id: id || "",
+    console.log("json:", res);
+    console.log("searchParams:", id);
+    url.searchParams.forEach((value, key) => {
+        console.log(key + ":" + value);
     });
-    console.log("paymentData", paymentData);
+    if (!res) {
+        console.log("webhook error !res");
+        return new Response("webhook", { status: 200, headers: { 'content-type': 'application/json' } });
+    }
+    if (res.topic === "payment") {
+
+        const paymentData = await payment.get({
+            id: id || "",
+        });
+        console.log("paymentData", paymentData);
+    }
+
+
     return new Response("webhook", { status: 200, headers: { 'content-type': 'application/json' } });
 }
